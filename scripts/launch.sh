eef40899e404e04102308309453f645b91cd6d24
Azamat <dzhunebaev@gmail.com>
2023-07-28 18:50:14 +0600

    deploy

--- scripts/launch.sh
+++ scripts/launch.sh
@@ -6,14 +6,11 @@
 : "${APP_MODULE:=$MODULE_NAME:$VARIABLE_NAME}"
 : "${HOST:=0.0.0.0}"
 : "${PORT:=8000}"
-: "${LOG_LEVEL:=info}"
-: "${LOG_CONFIG:=./deploy/configs/logging_uvicorn.ini}"
 
 # Start uvicorn with live-reload
 uvicorn \
     --proxy-headers \
     --host "$HOST" \
     --port "$PORT" \
-    --port "$LOG_CONFIG" \
     "$APP_MODULE"
     
